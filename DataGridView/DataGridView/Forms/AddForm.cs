4b3c0546160bf31abb4c0804061cd51c34c7afee
Train <otlichnik7236@gmail.com>
2025-10-24 19:32:09 +0300

    не до конца доделано, подправлю немножко

---
+++ DataGridView/DataGridView/Forms/AddForm.cs
@@ -0,0 +1,83 @@
+﻿using DataGridView.Clasess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace DataGridView.Forms
+{
+    public partial class AddForm : Form
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+        private MainForm _mainForm;
+        private int _editIndex = -1; // -1 = добавление, >=0 = редактирование
+
+        // Конструктор для добавления
+        public AddForm(MainForm main)
+        {
+            InitializeComponent();
+            _mainForm = main;
+        }
+
+        // Конструктор для редактирования
+        public AddForm(MainForm main, int index)
+        {
+            InitializeComponent();
+            _mainForm = main;
+            _editIndex = index;
+
+            Item item = Storage.Items[index];
+
+            txtName.Text = item.Name;
+            txtPrice.Text = item.Price.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtPrice.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+
+            if (!decimal.TryParse(txtPrice.Text, out decimal price))
+            {
+                MessageBox.Show("Цена должна быть числом!");
+                return;
+            }
+
+            if (_editIndex == -1)
+            {
+                // Добавление
+                Storage.Items.Add(new Item
+                {
+                    Name = txtName.Text,
+                    Price = price
+                });
+            }
+            else
+            {
+                // Изменение
+                Storage.Items[_editIndex].Name = txtName.Text;
+                Storage.Items[_editIndex].Price = price;
+            }
+
+            _mainForm.RefreshTable();
+            this.Close();
+        }
+        private void AddForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DataGridView/DataGridView/Forms/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using DataGridView.Forms;
+using DataGridView.Clasess;
+namespace DataGridView
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            RefreshTable();
+
+            // Добавляем события
+            dataGridView1.CellDoubleClick += dgvItems_CellDoubleClick;
+
+        }
+        public void RefreshTable()
+        {
+            dataGridView1.Rows.Clear();
+
+            foreach (var item in Storage.Items)
+            {
+                dataGridView1.Rows.Add(item.Name, item.Price);
+            }
+        }
+
+        // Открываем форму редактирования по двойному клику
+        private void dgvItems_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                AddForm editForm = new AddForm();
+                editForm.Show();
+            }
+        }
+
+        // Клик по кнопке "Добавить"
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddForm addForm = new AddForm();
+            addForm.Show();
+        }
+
+        // Клик по кнопке "Удалить"
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Выберите строку!");
+                return;
+            }
+
+            int index = dataGridView1.SelectedRows[0].Index;
+            Storage.Items.RemoveAt(index);
+
+            RefreshTable();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            AddForm addForm = new AddForm();
+            addForm.ShowDialog();
+        }
+    }
+}
